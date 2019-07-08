c74da12212c2645e4a74832d075f49baeafaba78
aryyya <aryyya.xyz@gmail.com>
2019-07-08 14:50:42 -0700

    Use mock data to render realistic products.

--- src/components/product-list/product-list.js
+++ src/components/product-list/product-list.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
 
+import products from './seed'
 import Product from '../product/product'
 
 class ProductList extends Component {
   render () {
     return (
       <div className="ui unstackable items">
-        <Product />
+        {products.map(product => (
+          <Product
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            description={product.description}
+            url={product.url}
+            votes={product.votes}
+            submitterAvatarUrl={product.submitterAvatarUrl}
+            productImageUrl={product.productImageUrl}
+          />
+        ))}
       </div>
     )
   }
--- src/components/product/product.js
+++ src/components/product/product.js
@@ -2,24 +2,34 @@ import React, { Component } from 'react'
 
 class Product extends Component {
   render () {
+    const {
+      id,
+      title,
+      description,
+      url,
+      votes,
+      submitterAvatarUrl,
+      productImageUrl
+    } = this.props
+
     return (
       <div className="item">
         <div className="image">
           <img
-            src="images/products/image-aqua.png"
+            src={productImageUrl}
             alt="A graphic of the item."
           />
         </div>
         <div className="middle aligned content">
           <div className="description">
-            <a href="/">Fort Knight</a>
-            <p>Authentic renaissance actors, delivered in just two weeks.</p>
+            <a href={url}>{title}</a>
+            <p>{description}</p>
           </div>
           <div className="extra">
             <span>Submitted by:</span>
             <img
               className="ui avatar image"
-              src="images/avatars/daniel.jpg"
+              src={submitterAvatarUrl}
               alt="The avatar of the user that posted the item."
             />
           </div>
